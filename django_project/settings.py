4e4cd582ba2ce79e97c5a01f1ab4060ab62fbf51
amazingabyrd <austinybyrdy@gmail.com>
2023-09-11 13:25:36 -0600

    Setting

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio',
+    'portfolio_app',
 ]
 # Add support for authenticating users
 AUTHENTICATION_BACKENDS = [
