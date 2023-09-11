4dc8d53b6604e8fd8f4f02dd518fa2faedb6842b
amazingabyrd <austinybyrdy@gmail.com>
2023-09-11 10:46:37 -0600

    Settings allow portfolio and Added Authentication Backend

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio',
 ]
-
+# Add support for authenticating users
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
