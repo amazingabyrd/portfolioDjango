291d619ce443bd97bd6c0f7ca4962f0407137cd1
amazingabyrd <austinybyrdy@gmail.com>
2023-09-15 13:13:07 -0600

    Portfolio app and configuration adds”

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # App Name
     'portfolio_app',
 ]
 # Add support for authenticating users
